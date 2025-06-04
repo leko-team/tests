1b9fa80a8eca5e3a6b6c3a5177b6b2fa43ff5cb5
k.oliveri <k.oliveri@leko.team>
2025-06-04 15:54:02 +0200

    Обновлён механизм bootstrap-подключения для универсальной поддержки

--- src/CreatesApplicationTrait.php
+++ src/CreatesApplicationTrait.php
@@ -4,6 +4,7 @@ namespace LekoTeam\Tests;
 
 use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Application;
+use RuntimeException;
 
 /**
  * Трейт создания приложения.
@@ -21,12 +22,29 @@ trait CreatesApplicationTrait
      */
     public function createApplication(): Application
     {
-        $app = require __DIR__.'/../bootstrap/app.php';
-
+        $app = require $this->getAppPath();
         $app->make(Kernel::class)->bootstrap();
 
         $this->setConnectionToApplication($app);
 
         return $app;
+    }
+
+    /**
+     * Возвращает абсолютный путь к файлу bootstrap/app.php
+     *
+     * @return string Полный путь
+     *
+     * @throws RuntimeException
+     */
+    protected function getAppPath(): string
+    {
+        $path = dirname(__DIR__, 4) . '/bootstrap/app.php';
+
+        if (!file_exists($path)) {
+            throw new RuntimeException("Не удалось найти bootstrap/app.php по пути: $path");
+        }
+
+        return $path;
     }
 }
